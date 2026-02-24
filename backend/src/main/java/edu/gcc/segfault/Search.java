803101d142ce5ce6fbaddbe3c941abfb7e6899b6
MicP <miciscool.funee@gmail.com>
2026-02-24 09:35:45 -0500

    Constructor created

--- backend/src/main/java/edu/gcc/segfault/Search.java
+++ backend/src/main/java/edu/gcc/segfault/Search.java
@@ -2,12 +2,21 @@ package edu.gcc.segfault;
 
 import java.util.ArrayList;
 import java.util.Stack;
+//import com.fasterxml.jackson.databind.JsonNode; only if using node stuff
+
 
 public class Search {
     private ArrayList<Course> originalResults;
     private Stack<ArrayList<Course>> history;
     private ArrayList<Filter> activeFilters;
     private ArrayList<String> searchKeywords;
+
+    public Search(){
+        this.originalResults = new ArrayList<>();
+        this.history = new Stack<>();
+        this.activeFilters = new ArrayList<>();
+        this.searchKeywords = new ArrayList<>();
+    }
 
     public ArrayList<Course> fetchQuery(){
         return new ArrayList<>();
