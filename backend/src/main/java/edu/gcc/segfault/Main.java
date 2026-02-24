afd792bf8c4e05d3b6474a6651c5d65dfecc1208
gideo <gideon.buehler@gmail.com>
2026-02-24 16:41:32 -0500

    Added comments for neccesary blocks

--- backend/src/main/java/edu/gcc/segfault/Main.java
+++ backend/src/main/java/edu/gcc/segfault/Main.java
@@ -10,10 +10,6 @@ import java.util.ArrayList;
 public class Main {
     private static ArrayList<Course> courses;
 
-    public static void main(String[] args) throws Exception {
-        new Main().run();
-    }
-
     public void run() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(new File("data_wolfe.json"));
@@ -21,11 +17,13 @@ public class Main {
     }
 
     private static ArrayList<Course> loadAll(JsonNode root) {
+        // Create arraylist for courses
         ArrayList<Course> courseList = new ArrayList<>();
 
         JsonNode classes = root.get("classes");
         if (classes == null) return courseList;
 
+        // Iterate through every course in classes
         for (JsonNode node : classes) {
             Course c = fromJson(node);
             if (c != null) courseList.add(c);
@@ -38,6 +36,7 @@ public class Main {
         if (node == null) return null;
 
         try {
+            // Get all the course info from JSON, made then ternaries in case the values dont exist
             String subject = node.has("subject") ? node.get("subject").asText() : "UNKNOWN";
             int number = node.has("number") ? node.get("number").asInt() : 0;
             String section = node.has("section") ? node.get("section").asText() : "?";
@@ -55,22 +54,24 @@ public class Main {
             int openSeats = node.has("open_seats") ? node.get("open_seats").asInt() : 0;
             int totalSeats = node.has("total_seats") ? node.get("total_seats").asInt() : 0;
 
+            // Faculty is an array, but there doesnt appear to be more than one prof. Named it professor so it's accurate to everything else
             String professor = "TBA";
             JsonNode faculty = node.get("faculty");
             if (faculty != null && !faculty.isEmpty()) {
                 professor = faculty.get(0).asText();
             }
-
+            // List of days
             ArrayList<String> days = new ArrayList<>();
             LocalTime startTime = null;
             LocalTime endTime = null;
 
             JsonNode times = node.get("times");
+            // Parse time as LocalTime object
             if (times != null && !times.isEmpty()) {
                 startTime = LocalTime.parse(times.get(0).get("start_time").asText());
                 endTime = LocalTime.parse(times.get(0).get("end_time").asText());
 
-                // For each timeslot add
+                // For each timeslot add, get the day and add it to days; ie M, W, F
                 for (JsonNode t : times) {
                     days.add(t.get("day").asText());
                 }
