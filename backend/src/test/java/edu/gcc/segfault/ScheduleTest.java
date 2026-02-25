1ff42ce27bee7ddb609d4b84bada18a2acd1fec5
JMWolverine <147189390+JMWolverine@users.noreply.github.com>
2026-02-25 13:52:36 -0500

    trying

--- backend/src/test/java/edu/gcc/segfault/ScheduleTest.java
+++ backend/src/test/java/edu/gcc/segfault/ScheduleTest.java
@@ -1,4 +1,15 @@
+package edu.gcc.segfault;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ScheduleTest {
-  
+
+    @Test
+    void checkConflicts() {
+        Schedule s = new Schedule();
+        s.addCourse(new Course());
+
+    }
 }
