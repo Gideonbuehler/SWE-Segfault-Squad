d46defd4eee0930ff7d2d58494d01c7419a7b037
MicP <miciscool.funee@gmail.com>
2026-02-25 12:40:29 -0500

    began search testing

--- backend/src/main/java/edu/gcc/segfault/Search.java
+++ backend/src/main/java/edu/gcc/segfault/Search.java
@@ -18,13 +18,44 @@ public class Search {
         this.searchKeywords = new ArrayList<>();
     }
 
-    public ArrayList<Course> fetchQuery(){
-        return new ArrayList<>();
+    /**
+     * @param searchKeywords
+     * @return an ArrayList of courses that have a similarity to the searchKeywords ArrayList
+     */
+    public ArrayList<Course> fetchQuery(ArrayList<String> searchKeywords){
+        //stub code return new ArrayList<>();
+
+        //Course code
+        //Course name
+        //Prof name
+        //Dept
+
+        ArrayList<Course> allCourses = Main.getCourses();
+
+        ArrayList<Course> query = new ArrayList<>();
+
+        for(int c = 0; c<allCourses.size(); c++){
+            Course toCheck = allCourses.get(c);
+            String code = toCheck.getCourseCode();
+            String name = toCheck.getCourseName();
+            String professor = toCheck.getProfessor();
+            String department = toCheck.getDepartment();
+            for(int k=0; k<searchKeywords.size(); k++){
+                if(code.equalsIgnoreCase(searchKeywords.get(k)) || name.equalsIgnoreCase(searchKeywords.get(k)) || professor.equalsIgnoreCase(searchKeywords.get(k)) || department.equalsIgnoreCase(searchKeywords.get(k))){
+                    query.add(toCheck);
+                    break;
+                }
+            }
+        }
+
+        history.push(query);
+        return query;
     }
 
     public boolean applyFilters(){
         return false;
     }
+
     public ArrayList<Course> getResults(){
         return new ArrayList<>();
     }
---
+++ backend/src/test/java/edu/gcc/segfault/SearchTest.java
@@ -0,0 +1,12 @@
+package edu.gcc.segfault;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchTest {
+
+    @Test
+    void fetchQuery() {
+    }
+}
