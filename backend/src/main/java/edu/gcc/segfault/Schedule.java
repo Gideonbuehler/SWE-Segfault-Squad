7d831f4d3a54dd795950a6de34fa1d5fe4bad66b
JMWolverine <147189390+JMWolverine@users.noreply.github.com>
2026-02-24 19:38:29 -0500

    Beginning algorithm for checkConflicts

--- backend/src/main/java/edu/gcc/segfault/Schedule.java
+++ backend/src/main/java/edu/gcc/segfault/Schedule.java
@@ -17,6 +17,18 @@ public class Schedule {
     }
 
     public boolean checkConflicts(Course toCheck){
+        for(Course c: courses){
+            for(String d: c.getDays()){
+                for(String d2: toCheck.getDays()){
+                    if(d.equals(d2)){
+                        if(c.getStartTime <= toCheck.getStartTime && c.getEndTime >= toCheck.getStartTime
+                    }
+                }
+            }
+        }
+
+
+
         return false;
     }
 
