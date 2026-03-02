ea71a69a8427c4745414f2e07f41ed9d15c1990e
MicP <miciscool.funee@gmail.com>
2026-03-02 16:32:32 -0500

    Inital dev on filters, copied method over from search as it will be slightly the same idea just different criteria. I do not know why the method is a boolean, the param for it should be the filters? Not a list of courses. I will hopefully get answers from inside my head otherwise I will be waiting to reconnect with the team after spring break.

--- backend/src/main/java/edu/gcc/segfault/Filter.java
+++ backend/src/main/java/edu/gcc/segfault/Filter.java
@@ -11,8 +11,35 @@ public class Filter {
     private LocalTime startTime;
     private LocalTime endTime;
 
+
+    /**
+     * This method retrieves the most recent search query (top of the stack named history in the Search class) and parses it over the filters specified by the user.
+     * This generates a new list which is returned and show to the suer, as well as pushed to the top of the history stack.
+     * @param courses
+     * @return
+     */
     public boolean applyFilters(ArrayList<Course> courses){
-        return false;
+
+        ArrayList<Course> allCourses = Search.history.peek();
+
+        ArrayList<Course> query = new ArrayList<>();
+
+        for(int c = 0; c<allCourses.size(); c++){
+            Course toCheck = allCourses.get(c);
+            String code = toCheck.getCourseCode();
+            String name = toCheck.getCourseName();
+            String professor = toCheck.getProfessor();
+            String department = toCheck.getDepartment();
+            for(int k=0; k<searchKeywords.size(); k++){
+                if(code.equalsIgnoreCase(searchKeywords.get(k)) || name.equalsIgnoreCase(searchKeywords.get(k)) || professor.equalsIgnoreCase(searchKeywords.get(k)) || department.equalsIgnoreCase(searchKeywords.get(k))){
+                    query.add(toCheck);
+                    break;
+                }
+            }
+        }
+
+        Search.history.push(query);
+        return true;
     }
 
 
