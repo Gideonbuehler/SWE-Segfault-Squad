44e4f8668fc74a69ca1c972dae2de23ae67da8e7
JMWolverine <147189390+JMWolverine@users.noreply.github.com>
2026-03-13 23:15:08 -0400

    Started working on exectuing queries

--- backend/src/main/java/edu/gcc/segfault/Supabase.java
+++ backend/src/main/java/edu/gcc/segfault/Supabase.java
@@ -1,9 +1,11 @@
 package edu.gcc.segfault;
 import io.github.cdimascio.dotenv.Dotenv;
+import net.bytebuddy.dynamic.scaffold.MethodRegistry;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.*;
 
 public class Supabase {
 
@@ -11,18 +13,30 @@ public class Supabase {
     private static final String URL = dotenv.get("DB_URL");
     private static final String USER = dotenv.get("DB_USER");
     private static final String PASSWORD = dotenv.get("DB_PSWRD");
+    private static Connection conn;
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println(System.getProperty("user.dir"));
         System.out.println(URL);
-        try (Connection conn = connect()) {
-            System.out.println("Connected to Supabase!");
+        try {
+            conn = connect();
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
+
+
+        String preparedStatement = "SELECT * FROM CourseOfferings WHERE code LIKE %Intro%";
+        PreparedStatement pstmt = conn.prepareStatement(preparedStatement);
+        Statement s = conn.createStatement();
+
+        ResultSet rs = pstmt.executeQuery();
+        while(rs.next()){
+            System.out.println("code: " + rs.getString("code") + rs.getString("number") + " course name: " + rs.getString("name"));
+        }
+
     }
 }
