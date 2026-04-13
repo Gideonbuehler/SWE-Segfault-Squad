89162eeaed6795b355a6af265e069322ccf8ee1c
JMWolverine <147189390+JMWolverine@users.noreply.github.com>
2026-04-12 20:40:14 -0400

    Need to add a custom/different message for if the class if full? Or on the front end just remove the add button?

--- backend/src/main/java/edu/gcc/segfault/Controller.java
+++ backend/src/main/java/edu/gcc/segfault/Controller.java
@@ -111,6 +111,7 @@ public class Controller {
                 ctx.result("Course conflict");
                 return;
             }
+            //Add specific message for full class?
         });
 
         app.delete("/mySchedule/remove/{courseCode}/{semester}", ctx -> {
