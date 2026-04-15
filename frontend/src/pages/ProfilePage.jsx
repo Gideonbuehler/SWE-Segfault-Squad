91595e0fd00e9c065274e78a49481b9c7fe8a850
JMWolverine <147189390+JMWolverine@users.noreply.github.com>
2026-04-15 13:15:04 -0400

    Form is started but cannot get the profile object to the .jsx file

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,9 +1,54 @@
+import {useState} from "react";
+
 function ProfilePage({ darkMode, setDarkMode }) {
+  const [profile, setProfile] = useState(null);
+  const [major, setMajor] = useState("");
+  const [minor, setMinor] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    fetch(`/api/profile/`)
+      .then(res => res.json())
+      .then(data => setProfile(data))
+      .catch(err => console.error(err));
+  }, []);
+
+
+  
   return (
     <div>
       <h1>Profile</h1>
       <div className="card">
+      <h2>Major: `${profile.major}`</h2>
+
+      </div>
+      <div className="card">
         <p>User information will appear here.</p>
+        <form onSubmit={handleSubmit}>
+          <label>Update Major</label>
+          <input 
+          type="text"
+          value={major}
+          onChange={(e) => setMajor(e.target.value)}
+          />
+          <label> Add Minor</label>
+          <input 
+          type="text"
+          value={minor}
+          onChange={(e) => setMinor(e.target.value)}
+          />
+          <label> Remove Minor</label>
+          <select 
+          value={minor}
+          onChange={(e) => setMinor(e.target.value)}
+          >
+            <option value="Choose a Minor to Remove"></option>
+
+            </select>
+        </form>
+          
       </div>
       <button
         onClick={() => setDarkMode(prev => !prev)}
