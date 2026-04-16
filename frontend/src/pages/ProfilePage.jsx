1a0a6ff116fab2e9533d037e65ae7156d7d40fe6
JMWolverine <147189390+JMWolverine@users.noreply.github.com>
2026-04-16 11:38:17 -0400

    Year correctly updates on Submit. Minor is in progress. Changed to Textbox. Still have to connect the backend.

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -3,27 +3,53 @@ import { useState, useEffect } from "react";
 function ProfilePage({ darkMode, setDarkMode }) {
   const [profile, setProfile] = useState(null);
   const[formData, setFormData] = useState(null);
-  const[y, setYear] = useState("");
-  const[ma, setMajor] = useState("");
-  const[mi, setMinor] = useState([]);
-  const[cc, setCompletedCourses] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
+
+     setProfile(prev => ({
+    ...prev,
+    year: formData.year
+  }));
     if (formData.major.trim() === "") return;
+
+
+   
+
 
   setProfile(prev => ({
     ...prev,
     major: formData.major
   }));
+
+  setProfile(prev => ({
+    ...prev,
+    minors: formData.minors
+  }));
   };
 
+  //if (formData.minors.trim() === "") return;
+  
    const fetchProfile = async () => {
     
     const response = await fetch(`/api/profile/`, { method: "GET" });
     const data = await response.json();
     setProfile(data);
-    setFormData(profile);
+    setFormData(data);
   };
+
+  const helperForMinors = async (toAddOrDelete) => {
+    const data = null;
+    if(formData.minors.includes(toAddOrDelete)){
+      const response = await fetch(`/api/minors/${toAddOrDelete}`, { method: "DELETE" });
+      data = await response.json();
+    }
+    else{
+      const response = await fetch(`/api/minors/${toAddOrDelete}`, { method: "POST" });
+      data = await response.json();
+    }
+    setProfile(data);
+    fetchProfile();
+  }
 
   useEffect(() => {
     fetchProfile();
@@ -33,6 +59,7 @@ function ProfilePage({ darkMode, setDarkMode }) {
     <div>
       <h1>Profile</h1>
       <div className="card">
+        <h1>Major: {profile?.year}</h1>
         <h1>Major: {profile?.major}</h1>
         <h1>Minors: {profile?.minors}</h1>
 
@@ -43,7 +70,7 @@ function ProfilePage({ darkMode, setDarkMode }) {
           <div style={{marginBottom: "7px"}}>
           <label> Change Year</label>
           <select
-          value={formData?.year || ""}
+          value={formData?.year}
           onChange={(e) => setFormData(prev => ({
       ...prev,
       year: e.target.value
@@ -51,10 +78,10 @@ function ProfilePage({ darkMode, setDarkMode }) {
   }
         > 
           <option value="">Choose a year</option>
-          <option value="FR">FRESHMAN</option>
-          <option value="SO">SOPHOMORE</option>
-          <option value="JR">JUNIOR</option>
-          <option value="SR">SENIOR</option>
+          <option value="FRESHMAN">FRESHMAN</option>
+          <option value="SOPHOMORE">SOPHOMORE</option>
+          <option value="JUNIOR">JUNIOR</option>
+          <option value="SENIOR">SENIOR</option>
           </select>
           </div>
           <br />
@@ -64,29 +91,21 @@ function ProfilePage({ darkMode, setDarkMode }) {
           type="text"
           value={formData?.major || ""}
            onChange={(e) => {
-            
-            
-            
               setFormData(prev => ({
       ...prev,
       major: e.target.value.toUpperCase()
     }));
-  }
-  
-  }
+  }}
           />
           </div>
         
           <br />
           <div style={{marginBottom: "7px"}}>
-          <label> Add Minor</label>
+          <label> Edit Minors</label>
           <input 
           type="text"
-          value={formData?.minor || ""}
-          onChange={(e) => setFormData(prev => ({
-      ...prev,
-      minor: e.target.value
-    }))
+          value={formData?.minors}
+          onChange={(e) => helperForMinors(e.target.value)
 
   }
 
